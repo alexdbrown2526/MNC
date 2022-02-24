17c13ea5617e18ea3b060a07a07a40325587e1bd
Alex Brown <alexdbrown2526@gmail.com>
2022-02-23 22:00:45 -0600

    stubbed out page routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,14 @@
+import Home from "./views/public/Home.svelte";
+import About from "./views/public/About.svelte";
+import Projects from "./views/public/Projects.svelte";
+import Services from "./views/public/Services.svelte";
+import Contact from "./views/public/Contact.svelte";
+
 const routes = [
   { name: "/", component: Home },
   { name: "/about", component: About },
+  { name: "/projects", component: Projects },
+  { name: "/services", component: Services },
   { name: "/contact", component: Contact },
 ];
 
